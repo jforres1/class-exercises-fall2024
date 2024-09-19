bfeb0844c1dc727f4dc4afa1eebdfb7216a4f795
jforres1 <jforres1@unca.edu>
2024-09-19 11:50:40 -0400

    Completed lab05, answers and poetry/node lab work.

---
+++ lab05/poetry-demo/lab05-experiments.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+def main():
+    print("hello world")
+    # user_agent makes it seem like the request is coming from a web browser (versus a bot)
+    user_agent = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get("https://new.cs.unca.edu/", headers=user_agent)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    for link in soup.find_all("a"):
+        print("Link: ", link.get("href"), "Text: ", link.string)
+    #print(response.content)
+
+if __name__ == "__main__":
+    main()
