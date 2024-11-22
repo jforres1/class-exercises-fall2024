9215ee4786e741698aeaa39cbbd83e26d38fd766
jforres1 <jforres1@unca.edu>
2024-11-21 21:43:00 -0500

    Work implementing carousel, some errors are present in the code.

--- lab08/App.jsx
+++ lab08/App.jsx
@@ -1,61 +1,56 @@
-import React from "react";
-
-import { Image } from "antd";
-import { Carousel } from 'antd';
+import React, {useState} from "react";
+import { Carousel, Button, Input } from 'antd';
 
 export default function App() {
+
+    const [artist, setArtist] = useState('');
+    const [limit, setLimit] = useState(20);
+    const [tracks, setTracks] = useState([]);
+
     const contentStyle = {
         "width": "640px", 
         "border": "solid 1px #000", 
         "margin": "auto"
     };
-    const albums = [
-        {
-           "id": "6BzxX6zkDsYKFJ04ziU5xQ",
-           "name": "COWBOY CARTER",
-           "image_url": "https://i.scdn.co/image/ab67616d0000b2731572698fff8a1db257a53599",
-           "spotify_url": "https://open.spotify.com/album/6BzxX6zkDsYKFJ04ziU5xQ"
-        },
-        {
-           "id": "2UJwKSBUz6rtW4QLK74kQu",
-           "name": "BEYONCÉ [Platinum Edition]",
-           "image_url": "https://i.scdn.co/image/ab67616d0000b2730d1d6e9325275f104f8e33f3",
-           "spotify_url": "https://open.spotify.com/album/2UJwKSBUz6rtW4QLK74kQu"
-        },
-        {
-           "id": "6PeoltoiWQWCyWA0JBHVGN",
-           "name": "16 CARRIAGES",
-           "image_url": "https://i.scdn.co/image/ab67616d0000b273f5220893852002a2a3078bab",
-           "spotify_url": "https://open.spotify.com/album/6PeoltoiWQWCyWA0JBHVGN"
-        },
-        {
-           "id": "6oxVabMIqCMJRYN1GqR3Vf",
-           "name": "Dangerously In Love",
-           "image_url": "https://i.scdn.co/image/ab67616d0000b27345680a4a57c97894490a01c1",
-           "spotify_url": "https://open.spotify.com/album/6oxVabMIqCMJRYN1GqR3Vf"
-        },
-        {
-           "id": "2m1enA3YrMLVvR3q0MqLpL",
-           "name": "COWBOY CARTER",
-           "image_url": "https://i.scdn.co/image/ab67616d0000b2734903a9678d5664b9cd9a3fd8",
-           "spotify_url": "https://open.spotify.com/album/2m1enA3YrMLVvR3q0MqLpL"
-        }
-    ]; 
-    function albumToJSX(albumJSON){
+
+    function trackToJSX(trackJSON){
+        const src_url = "https://open.spotify.com/embed/track/" + trackJSON.id + "?utm_source=generator" 
         return (
-            <div key = {albumJSON.id}>
-                <img src = {albumJSON.image_url} />
-                <h3>{albumJSON.name}</h3>
-            </div>
+            <iframe
+                src={src_url}
+                width="100%" 
+                border="0"
+                height="352" 
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                loading="lazy">
+            </iframe>
         )
-    }        
+    }
+
+    async function getServerData(artist, limit) {
+        const base_url = "https://www.apitutor.org/spotify/simple/v1/search";
+        const extended_url = base_url + "?=" + artist + "&type=track&limit=" + limit;
+        const response = await fetch(extended_url);
+        const data = await response.json();
+        setTracks(data);
+    }
+
     return (
-        <div style = {contentStyle}>
-            <Carousel dotPosition="top">
-                {
-                    albums.map(albumToJSX)
-                }
-            </Carousel>
-        </div>
+        <>
+            <Input
+                value={artist}
+                onChange={e => setArtist(e.target.value)} />
+            <Input
+                value={limit}
+                onChange={e => setLimit(e.target.value)} />
+            <Button onClick={getServerData(artist, limit)}>
+                Submit
+            </Button>
+            <div style={contentStyle}>
+                <Carousel dotPosition="top">
+                    {tracks.map(trackToJSX)}
+                </Carousel>
+            </div>
+        </>
     );
 }
